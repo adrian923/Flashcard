6c21ab0961e68740e1883d897dd0534be8c55bc4
Adrian_GK <133013101+adrian923@users.noreply.github.com>
2024-10-17 07:33:45 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'programacao',
+    'o que e python?',
+    'o python e uma linguagem de programacao'
+    )
+
+criaCartao(
+    'geografia'
+    'qual a capital da Franca'
+    'a capital da Franca e Paris'
+)
+
+criaCartao(
+    'programacao'
+    'o que e uma funcao?'
+    'uma funcao e um bloco de codigo que executa alguma tarefa'
+)
+
+criaCartao(
+    'lingua inglesa'
+    'como se diz oi em ingles?'
+    'oi em ingles e HI (RAI)'
+)
